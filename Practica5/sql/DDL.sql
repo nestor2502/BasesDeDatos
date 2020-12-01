126d28612bf4608ef484d392b5dcb0fe52b10af8
nestor2502 <nestor2502@ciencias.unam.mx>
2020-11-30 20:11:46 -0600

    parte de juan

--- Practica5/sql/DDL.sql
+++ Practica5/sql/DDL.sql
@@ -36,7 +36,7 @@ CREATE TABLE "fbd2021_1_1625".horario(
 	(
 		"id_horario"
 	),
-	CONSTRAINT "fk_usuario_horario" FOREIGN KEY
+	CONSTRAINT "fk_ousuari_horario" FOREIGN KEY
 	(
 		"id_persona"
 	) REFERENCES "fbd2021_1_1625".usuario (
@@ -44,3 +44,27 @@ CREATE TABLE "fbd2021_1_1625".horario(
 	) 
 
 );
+
+CREATE TABLE "fbd2021_1_1625".taxi(
+	"placa" varchar(7) NOT NUll,
+	"id_persona" int NOT NULL,
+	"modelo" varchar(20) NOT NULL,
+	"marca" varchar(20) NOT NULL,
+	"anio" smallint NOT NULL, 
+	"no_cilindros" smallint NOT NULL CHECK(no_cilindros > 0),
+	"no_puertas" smallint NOT NULL CHECK(no_puertas > 1),
+	CONSTRAINT "pk_taxi" PRIMARY KEY("placa"),
+	CONSTRAINT "pk_idpersona" FOREIGN KEY ("id_persona") 
+	REFERENCES "fbd2021_1_1625".asociado("id_persona")
+	);
+
+CREATE TABLE "fbd2021_1_1625".estado(
+	"id_estado" int GENERATED ALWAYS AS IDENTITY,
+	"placa" varchar(7),
+	"id_persona" int,
+	"razon" text,
+	"activo" boolean,
+	CONSTRAINT "pk_idestado" PRIMARY KEY("id_estado"),
+	CONSTRAINT "fk_placa" FOREIGN KEY("placa") REFERENCES "fbd2021_1_1625".taxi("placa"),
+	CONSTRAINT "pk_idpersona" FOREIGN KEY("id_persona") REFERENCES "fbd2021_1_1625".asociado("id_persona")
+	);
